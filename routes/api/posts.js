7db59e1d8e9a953814474af0afc5a304c2ab88cf
Eden Park <edenGoforit@gmail.com>
2021-03-30 21:53:47 +1300

    Feat: add and delete comments post methods

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -81,6 +81,7 @@ router.post(
 // @desc     Like Post
 // @access   Private
 //why session:false? http://www.passportjs.org/docs/authenticate/
+//After successful authentication, Passport will establish a persistent login session. This is useful for the common scenario of users accessing a web application via a browser.
 router.post(
   "/like/:id",
   passport.authenticate("jwt", { session: false }),
@@ -110,7 +111,6 @@ router.post(
 // @route    Post api/posts/unlike/:id
 // @desc     Unlike Post
 // @access   Private
-//why session:false? http://www.passportjs.org/docs/authenticate/
 router.post(
   "/unlike/:id",
   passport.authenticate("jwt", { session: false }),
@@ -139,6 +139,59 @@ router.post(
       .catch((err) => res.status(404).json({ erorr: "Post Not Found" }));
   }
 );
- 
+
+// @route   POST api/posts/commnet/:id
+// @desc    Add comment to post
+// @access  Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then((post) => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id,
+        };
+        //Add comment to the top of the array
+        post.comments.unshift(newComment);
+        post.save().then((savedPost) => res.json(savedpost));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// @route   POST api/posts/commnet/:id/:comment)id
+// @desc    Delete comment to post
+// @access  Private
+router.post(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then((post) => {
+        if (
+          post.comments.filter(
+            (comment) => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res.status(404).json({ error: "Comment does not exist" });
+        }
+        const removeIndex = post.comments
+          .map((item) => item._id.toString())
+          .indexOf(req.params.comment_id);
+        post.comments.splice(removeIndex, 1);
+        post.save().then((savedPost) => res.json(savedPost));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
 
 module.exports = router;
