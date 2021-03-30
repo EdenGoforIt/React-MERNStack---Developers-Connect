be16eefa5dfe24653154ff84bede274913bb8bd7
Eden Park <2015015407@student.sit.ac.nz>
2021-03-30 21:18:42 +1300

    Feat: post like and unlike methods

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -31,7 +31,7 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)
-    .then((posts) => res.json(posts))
+    .then((post) => res.json(posts))
     .catch((err) => res.status(404).json({ error: "No Post Found" }));
 });
 
@@ -74,6 +74,69 @@ router.post(
       user: req.user.id,
     });
     newPost.save().then((post) => res.json(post));
+  }
+);
+
+// @route    Post api/posts/likes/:id
+// @desc     Like Post
+// @access   Private
+//why session:false? http://www.passportjs.org/docs/authenticate/
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        Post.findById(req.params.id).then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ error: "User already liked this post" });
+          }
+          //add to the post
+          post.likes.unshift({ user: req.user.id });
+          post.save().then((post) => {
+            res.json(post);
+          });
+        });
+      })
+      .catch((err) => res.status(404).json({ erorr: "Post Not Found" }));
+  }
+);
+
+// @route    Post api/posts/unlike/:id
+// @desc     Unlike Post
+// @access   Private
+//why session:false? http://www.passportjs.org/docs/authenticate/
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        Post.findById(req.params.id).then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ error: "You have not yet liked this post" });
+          }
+          //get remove index
+
+          const removeIndex = post.likes
+            .map((item) => item.user.toString())
+            .indexOf(req.user.id);
+          //splice from the array
+          post.likes.splice(removeIndex, 1);
+          post.save().then((savedPost) => res.json(savedPost));
+        });
+      })
+      .catch((err) => res.status(404).json({ erorr: "Post Not Found" }));
   }
 );
 
