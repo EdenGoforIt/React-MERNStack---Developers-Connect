1fa4bbc6b896927825128a680324afa91d7ece19
Eden Park <edenGoforit@gmail.com>
2021-04-08 20:18:27 +1200

    Feat: components of login and register set up, routes for them have been set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,23 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
@@ -16,12 +17,12 @@ class Landing extends Component {
                     help from other developers
                   </p>
                   <hr />
-                  <a href="register.html" className="btn btn-lg btn-info mr-2">
+                  <Link to="/register" className="btn btn-lg btn-info mr-2">
                     Sign Up
-                  </a>
-                  <a href="login.html" className="btn btn-lg btn-light">
+                  </Link>
+                  <Link to="/login" className="btn btn-lg btn-light">
                     Login
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
