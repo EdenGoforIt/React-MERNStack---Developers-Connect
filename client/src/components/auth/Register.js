0efd6906afd340a5d918ee47bc3240c755c18f87
Eden Park <edenGoforit@gmail.com>
2021-04-11 11:57:28 +1200

    Feat:Login page html and state

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -34,9 +34,7 @@ class Register extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">
-                Create your DevConnector account
-              </p>
+              <p className="lead text-center">Create your account</p>
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
