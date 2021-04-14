668a0d008f288cdf3c25de324e2ce70c4a3ca9f2
Eden Park <edenGoforit@gmail.com>
2021-04-14 21:25:20 +1200

    Feat: default store setting

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
 
 //reducers, initial state and middles wares
+const middleware = [thunk];
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
 
 export default store;
