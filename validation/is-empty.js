ea800ef5b88ce58b4de7e9055355ba774fe99d4f
Eden Park <edenGoforit@gmail.com>
2021-06-01 21:58:53 +1200

    Fix: empty validation check type of Object to 'object'

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -2,7 +2,7 @@ const isEmpty = (value) => {
   return (
     value === undefined ||
     value === null ||
-    (typeof value === "Object" && Object.keys(value).length === 0) ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
     (typeof value === "string" && value.trim().length === 0)
   );
 };
