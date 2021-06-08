8b540b3432d7f9e85eac209b1be74a2b7c3421d0
Eden Park <edenGoforit@gmail.com>
2021-06-08 21:46:25 +1200

    Feat: expire the login session after assigned time

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,17 @@ import { Provider } from "react-redux";
 import store from "./store";
 import setAuthToken from "./utils/setAuthToken";
 import jwt_decode from "jwt-decode";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login'";
+  }
 }
 
 function App() {
