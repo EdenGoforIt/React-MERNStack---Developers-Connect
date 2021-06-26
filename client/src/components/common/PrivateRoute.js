7d88a3d694d514cdcaed7f4ff831a05a91b5a0ab
Eden Park <edenGoforit@gmail.com>
2021-06-26 15:21:59 +1200

    Fix: Private route fixed
    {} to () in render

--- client/src/components/common/PrivateRoute.js
+++ client/src/components/common/PrivateRoute.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import PropType from "prop-types";
+import PropTypes from "prop-types";
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
   <Route
     {...rest}
-    render={(props) => {
-      auth.isAuthenticated ? (
+    render={(props) =>
+      auth.isAuthenticated === true ? (
         <Component {...props} />
       ) : (
         <Redirect to="/login" />
-      );
-    }}
-  />;
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
+
 export default connect(mapStateToProps)(PrivateRoute);
